0fe83e1161d2172fa7175064314c837128b766cf
isayme <isaymeorg@gmail.com>
2018-07-04 18:29:38 +0800

    update request-ip to 2.1.1

--- app/middleware/ip.js
+++ app/middleware/ip.js
@@ -1,11 +1,5 @@
 const requestIP = require('request-ip')
-const express = require('express')
 
-// get ip with request-ip
-Object.defineProperty(express.request, 'ip', {
-  configurable: true,
-  enumerable: true,
-  get: function ip () {
-    return requestIP.getClientIp(this)
-  }
+module.exports = requestIP.mw({
+  attributeName: 'ip'
 })
